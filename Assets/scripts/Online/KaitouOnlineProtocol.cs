bd0c1f7d1393a6f3c995f67a10efd27b986fede1
kaitooougames <kaitooougame@gmail.com>
2026-08-01 01:51:18 +0900

    Use local action reveal sequence online

--- Assets/scripts/Online/KaitouOnlineProtocol.cs
+++ Assets/scripts/Online/KaitouOnlineProtocol.cs
@@ -11,7 +11,6 @@ namespace KaitouOnline
         StartGame,
         ActionRequest,
         StateSnapshot,
-        RevealActions,
         SecurityDiceState,
         TreasureDisplayChoice,
         TreasureStealChoice,
@@ -166,7 +165,7 @@ namespace KaitouOnline
     {
         // オンライン進行規約。古いMacビルドとの混在を防ぐため、
         // フェーズ同期方式を変更したら必ず更新する。
-        public const int Version = 6;
+        public const int Version = 7;
         public const string MessageName = "KaitouOnlineEvent";
 
         public static string Json<T>(T value) => JsonUtility.ToJson(value);
