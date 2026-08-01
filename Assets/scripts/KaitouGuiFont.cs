4b574f7f1266ef5d8e01c66566ca119468d2b27c
kaitooougames <kaitooougame@gmail.com>
2026-08-01 21:45:59 +0900

    Use bundled Japanese font in Unity Editor

--- Assets/scripts/KaitouGuiFont.cs
+++ Assets/scripts/KaitouGuiFont.cs
@@ -1,10 +1,15 @@
 using System;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 /// <summary>WebGLでもIMGUIの日本語を表示するための共通フォント設定。</summary>
 public static class KaitouGuiFont
 {
     private const string JapaneseFontName = "SourceHanSansJP-Regular";
+    private const string JapaneseFontPath =
+        "Assets/TextMesh Pro/Fonts/SourceHanSansJP-Regular.otf";
     private static Font japaneseFont;
     private static bool searched;
 
@@ -16,10 +21,15 @@ public static class KaitouGuiFont
             japaneseFont = Array.Find(Resources.FindObjectsOfTypeAll<Font>(),
                 font => font != null && font.name == JapaneseFontName);
 
-#if !UNITY_WEBGL
+#if UNITY_EDITOR
+            // EditorではOSフォント名へフォールバックせず、プロジェクト内の
+            // 日本語フォントを直接読み込む。
+            if (japaneseFont == null)
+                japaneseFont = AssetDatabase.LoadAssetAtPath<Font>(JapaneseFontPath);
+#elif !UNITY_WEBGL
             if (japaneseFont == null)
                 japaneseFont = Font.CreateDynamicFontFromOSFont(
-                    new[] { "Hiragino Sans", "Yu Gothic", "Noto Sans CJK JP" }, 18);
+                    new[] { "Hiragino Kaku Gothic ProN", "Yu Gothic", "Noto Sans CJK JP" }, 18);
 #endif
         }
 
